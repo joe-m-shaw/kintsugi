ee4546e07c5e6860095b9defd9cce1df85182dc6
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2025-10-07 10:49:42 +0100

    Add filename to warning messages

--- R/read-seqone-gi-csv.R
+++ R/read-seqone-gi-csv.R
@@ -38,49 +38,51 @@ read_seqone_gi_csv <- function(filepath,
                          levels = c("Positive", "Negative",
                                     "Non-conclusive"))) 
   
+  filename <- basename(filepath)
+  
   if(anyNA.data.frame(output |> 
                       dplyr::select(-c(brca_status, brca_mutation)))){
-    warning("NAs in output")
+    warning(paste0("NAs in output: ", filename))
   }
   
   if((min(output$lga, na.rm = TRUE) < 0) |
      (max(output$lga, na.rm = TRUE) > 100)){
-    warning("LGA value is not in range 0-100")
+    warning(paste0("LGA value is not in range 0-100: ", filename))
   }
   
   if((min(output$lpc, na.rm = TRUE) < 0)|
      (max(output$lpc, na.rm = TRUE) > 100)){
-    warning("LPC is not in range 0-100")
+    warning(paste0("LPC is not in range 0-100: ", filename))
   }
   
   if((min(output$score, na.rm = TRUE) < 0)|
      (max(output$score, na.rm = TRUE) > 1)){
-    warning("SeqOne score is not between between 0-1")
+    warning(paste0("SeqOne score is not between between 0-1: ", filename))
   } 
   
   if(min(output$ccne1_cn, na.rm = TRUE) < 0 |
      max(output$ccne1_cn, na.rm = TRUE) > 100){
-    warning("CCNE1 copy number is not between 0-100")
+    warning(paste0("CCNE1 copy number is not between 0-100: ", filename))
   } 
   
   if(min(output$rad51b_cn, na.rm = TRUE) < 0 |
      max(output$rad51b_cn, na.rm = TRUE) > 100){
-    warning("RAD51B copy number is not between 0-100")
+    warning(paste0("RAD51B copy number is not between 0-100: ", filename))
   }
   
   if(min(output$coverage) < 0 |
      max(output$coverage) > 7){
-    warning("Coverage is not between 0-7X")
+    warning(paste0("Coverage is not between 0-7X: ", filename))
   }
   
   if(min(output$pct_tum_cell) < 0 |
      max(output$pct_tum_cell) > 1){
-    warning("Percentage tumours cells is not between 0-1")
+    warning(paste0("Percentage tumours cells is not between 0-1: ", filename))
   }
   
   if(min(output$gi_confidence, na.rm = TRUE) < 0 |
      max(output$gi_confidence, na.rm = TRUE) > 1){
-    warning("GI confidence is not between 0-1")
+    warning(paste0("GI confidence is not between 0-1: ", filename))
   }
   
   return(output)
