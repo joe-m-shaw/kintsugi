cbbb4bb1b0048ba4bbe0ed0cc7c585de46521b36
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2025-10-08 09:21:50 +0100

    Rename find_worksheet_filepaths to list_worksheet_filepaths

--- R/list-worksheet-filepaths.R
+++ R/list-worksheet-filepaths.R
@@ -1,10 +1,10 @@
-find_worksheet_filepaths <- function(worksheet,
+list_worksheet_filepaths <- function(worksheet,
                                      path = "S:/central shared/Genetics/Repository/WorksheetAnalysedData/",
                                      pattern,
                                      recursive = TRUE,
                                      full_names = TRUE) {
   
-  #' Find filepaths for files within worksheet folders 
+  #' List filepaths for files within worksheet folders 
   #'
   #' @param worksheet Worksheet. Format "WS123456"
   #' @param path The filepath to the S drive "WorksheetAnalysedData" repository
@@ -12,10 +12,10 @@ find_worksheet_filepaths <- function(worksheet,
   #' @param recursive To be used by `list.files`. `TRUE` or `FALSE.` 
   #' @param full_names To be used by `list.files`. `TRUE` or `FALSE.` 
   #'
-  #' @returns A list fo worksheet filepaths
+  #' @returns A list of worksheet filepaths
   #' @export
   #'
-  #' @examples find_worksheet_filepaths(worksheet = "WS157517", pattern = "hrd-results.*csv")
+  #' @examples list_worksheet_filepaths(worksheet = "WS157517", pattern = "hrd-results.*csv")
   
   output <- list.files(paste0(path, worksheet, "/"),
                        full.names = full_names,
