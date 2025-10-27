63940d52f0cc0b67000092012c5b9ef731fcf57b
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2025-10-27 14:26:58 +0000

    Add tests for descriptions_* functions

---
+++ tests/testthat/test-descriptions-pansolid.R
@@ -0,0 +1,5 @@
+test_that("descriptions_pansolid returns correct number of strings", {
+  
+  expect_true(length(descriptions_pansolid()) >= 129)
+  
+})
---
+++ tests/testthat/test-descriptions-seqone-gi.R
@@ -0,0 +1,5 @@
+test_that("descriptions_seqone_gi returns correct number of strings", {
+  
+  expect_true(length(descriptions_seqone_gi()) >= 25)
+  
+})
