b7811c9d8839fd68e46819173972248e16ece4e8
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2025-10-29 16:01:58 +0000

    Add checks to connect_to_eval_hrd

--- R/connect-to-eval-hrd.R
+++ R/connect-to-eval-hrd.R
@@ -10,7 +10,12 @@ connect_to_eval_hrd <- function(
                    "test_name", 
                    "field", 
                    "data_value", 
-                   "box")) {
+                   "box"),
+    test_names = c("PANEL: M2.5 - SeqOne HRD Status",
+                    "PANEL: R207.1 - Inherited ovarian cancer (without breast cancer) v4.0 (ICP)",
+                    "PANEL: M2_tBRCA_PS",
+                    "PANEL: M2_tBRCA_PS v2.a",
+                    "PANEL: M2_tBRCA_PS v2.b")) {
   
   #' Connect to the Eval_HRD table
   #'
@@ -21,10 +26,11 @@ connect_to_eval_hrd <- function(
   #' @param catalog The catalog to select the lazy table from
   #' @param schema The schema to select the lazy table from
   #' @param table The name of the table containing iGene HRD test results.
-  #'
   #' @param col_vector Character vector of columns to select. This defaults to an
   #' order which seems to work. For some reason test_order_date should be 
   #' selected first to avoid an "Invalid Descriptor Index" error
+  #' @param test_names The names of analysis panels which are expected within this
+  #' table
   #'
   #' @returns The Eval_HRD table as a dataframe
   #' @export
@@ -38,6 +44,14 @@ connect_to_eval_hrd <- function(
   output <- lazy_tbl |> 
     dplyr::select({{ col_vector }}) |> 
     dplyr::collect()
+
+  if(nrow(output) == 0){
+    warning("Eval_HRD has 0 rows")
+  }
+  
+  if(length(setdiff(unique(output$test_name), test_names)) != 0){
+    warning("There are unexpected panels in the Eval_HRD table")
+  }
   
   return(output)
   
